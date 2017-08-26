6cf291b02e373b23db8ea2ef45cef5a365211e26
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2017-08-26 19:33:39 +0200

    Use file-embed package to load resources. #148
    The use of `makeRelativeToProject` might make the
    library easier to install with stack in some cases.
    
    Do not use QuasiQuotes, but ordinary TemplateHaskell.

--- src/Foreign/JavaScript/Include.hs
+++ src/Foreign/JavaScript/Include.hs
@@ -2,34 +2,17 @@
 module Foreign.JavaScript.Include (include) where
 
 import           Data.Functor
-import qualified Language.Haskell.TH       as TH
-import           Language.Haskell.TH.Quote
+import           Data.FileEmbed      (makeRelativeToProject)
+import           Language.Haskell.TH
 import           System.IO
-import           System.Environment(lookupEnv)
-import           Data.Maybe (fromMaybe)
 
-
-#if defined(SAMPLES)
-root :: FilePath
-root = ".." -- we are running the examples from ghci
-#else
-root :: FilePath
-root = "."
-#endif
-
-include :: QuasiQuoter
-include = QuasiQuoter
-        { quoteExp  = f             -- only used as an expression,
-        , quotePat  = undefined     -- hence all other use cases undefined
-        , quoteType = undefined
-        , quoteDec  = undefined
-        }
-    where
-    f s = TH.LitE . TH.StringL <$> TH.runIO (readFileUTF8 s)
+include :: FilePath -> Q Exp
+include path = do
+    path <- makeRelativeToProject path
+    LitE . StringL <$> runIO (readFileUTF8 path)
 
 readFileUTF8 :: FilePath -> IO String
 readFileUTF8 path = do
-    env <- lookupEnv "THREEPENNY_ROOT"
-    h <- openFile (fromMaybe root env ++ "/" ++ path) ReadMode
+    h <- openFile path ReadMode
     hSetEncoding h utf8
     hGetContents h
--- src/Foreign/JavaScript/Resources.hs
+++ src/Foreign/JavaScript/Resources.hs
@@ -1,23 +1,24 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Foreign.JavaScript.Resources where
 
 import           Data.Text                          (Text)
 import qualified Data.Text                  as Text
 import           Foreign.JavaScript.Include
 
-jsDriverCode :: Maybe String -> Text
-jsDriverCode dict = Text.unlines $ map Text.pack
-    [ [include|js/lib/jquery.js|]
-    , [include|js/lib/jquery-cookie.js|]
-    , "var Haskell = { };" ++ maybe "" (\dict -> "dict = \""++ dict++ "\";") dict
-    , [include|js/comm.js|]
-    , [include|js/ffi.js|]
-    , [include|js/lib.js|]
-    , [include|js/log.js|]
+
+jsDriverCode :: Text
+jsDriverCode = Text.unlines $ map Text.pack
+    [ $(include "js/lib/jquery.js")
+    , $(include "js/lib/jquery-cookie.js")
+    , "var Haskell = {};" ++ maybe "" (\dict -> "dict = \""++ dict++ "\";") dict
+    , $(include "js/comm.js")
+    , $(include "js/ffi.js")
+    , $(include "js/lib.js")
+    , $(include "js/log.js")
     ]
 
 cssDriverCode :: Text
-cssDriverCode = Text.pack [include|js/haskell.css|]
+cssDriverCode = Text.pack $(include "js/haskell.css")
 
 defaultHtmlFile :: Text
-defaultHtmlFile = Text.pack [include|js/index.html|]
+defaultHtmlFile = Text.pack $(include "js/index.html")
